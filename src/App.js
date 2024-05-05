fde3f91280389ac0545c9282109379902c7061f9
vivek kumar <vivek880490@gmail.com>
2024-05-05 19:48:58 +0530

    todoApp game checkpoint 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import Header from "./Header/Header.js";
+import List from "./Header/List/List.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <Header/>
+    <hr/>
+    <List/>
     </div>
   );
 }
---
+++ src/Header/List/List.js
@@ -0,0 +1,8 @@
+export default function List() {
+    return (
+
+        <p className="List">Nothing to do buddy. Sleep!!</p>
+      
+    );
+  }
+  
